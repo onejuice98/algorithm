87da93f5bbae211cc683878e4a6144f9276b2f70
onejuice98 <waterpurifier@khu.ac.kr>
2023-04-25 23:34:04 +0900

    baekjoon

---
+++ unsolved/1009.py
@@ -0,0 +1,25 @@
+import sys
+
+T = int(sys.stdin.readline())
+
+for _ in range(T):
+    a, b = map(int, (sys.stdin.readline()).split())
+    temp = a % 10
+    
+    if temp == 0:
+        print(10)
+    elif temp in [1, 5, 6]:
+        print(temp)
+    elif temp in [4, 9]:
+        temp_b = b % 2
+        if temp_b == 0:
+            print(temp * temp % 10)
+        else:
+            print(temp)
+    else:
+        temp_b = b % 4
+        if temp_b == 0:
+            print(temp ** 4 % 10)
+        else:
+            print(temp ** temp_b % 10)
+    
