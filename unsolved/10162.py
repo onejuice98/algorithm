ac7651def80dcf69e3ca758b0dc8307d00808a82
onejuice98 <waterpurifier@khu.ac.kr>
2023-05-08 23:46:01 +0900

    10162-3

--- unsolved/10162.py
+++ unsolved/10162.py
@@ -1,25 +1,25 @@
-# import sys
+import sys
 
-# T = int(sys.stdin.readline())
+T = int(sys.stdin.readline())
 
-# A = 300
-# B = 60
-# C = 10
+A = 300
+B = 60
+C = 10
 
-# answer = [0, 0, 0]
-# while T >= 10:
-#     if T > A:
-#         answer[0] += 1
-#         T -= A
-#     else:
-#         if T > B:
-#             answer[1] += 1
-#             T -= B
-#         else:
-#             answer[2] += 1
-#             T -= C
+answer = [0, 0, 0]
+while T >= 10:
+    if T > A:
+        answer[0] += 1
+        T -= A
+    else:
+        if T > B:
+            answer[1] += 1
+            T -= B
+        else:
+            answer[2] += 1
+            T -= C
             
-# if T != 0:
-#     print(-1)
-# else:
-#     print(*answer)
+if T != 0:
+    print(-1)
+else:
+    print(*answer)
