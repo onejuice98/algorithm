d37a539c42bc481c75a69835f5258a3f8b5b5d4a
onejuice98 <waterpurifier@khu.ac.kr>
2023-05-13 23:09:11 +0900

    greedy-

--- playgrond.py
+++ playgrond.py
@@ -1,19 +1,34 @@
-def solution(n, m, section):
-    floor = [True] * n
-    for i in section:
-        floor[i-1] = False
-    
-    cnt = 0
-    cur = 0
-    while not all(floor) and cur < len(section):
-        cursor = section[cur] -1
-        if not floor[cursor]:
-            for i in range(cursor, cursor+m):
-                floor[i] = True
-            cnt += 1
-        cur += 1
-        
-        
-    return cnt
+def solution(ages):
+    age=''
+    candle=[0,0,0,0,0,0,0,0,0,0]
+    for i in range(len(ages)):
+        age+=str(ages[i])
+    age=list(age)
+    for i in range(len(age)):
+        if age[i]=='6':
+            age[i]='9'
+    for i in age:
+        candle[int(i)]+=1
+        if candle[9]%2==0:
+            candle[9]=candle[9]//2
+        else:
+            candle[9]=candle[9]//2+1       
+    return max(candle)
 
-print(solution(8,4,[2, 3, 6]))
+ages = [6, 9, 6]
+
+needed = {}
+for age in ages:
+    for char in str(age):
+        if char not in needed:
+            if char == "6" or char == "9":
+                needed[char] = 0
+            else:
+                needed[char] = 1
+        else:
+            needed[char] += 1
+
+if max(needed.values()) == 0:
+    print(1)
+else: 
+    print(max(needed.values()))
