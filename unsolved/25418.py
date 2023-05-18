26dc3ef51a5b5e0002d62ff4e957e9ff419ba7b3
onejuice98 <waterpurifier@khu.ac.kr>
2023-05-18 23:45:35 +0900

    implement

--- unsolved/25418.py
+++ unsolved/25418.py
@@ -1,17 +1,17 @@
-import sys
+# import sys
 
-A, K = map(int, sys.stdin.readline().split())
-ans = 0
-while True:
-    if A == K:
-        print(ans)
-        break
-    else:
-        if K // 2 >= A:
-            if K % 2 == 0:
-                K = K // 2
-            else:
-                K -= 1
-        elif K - 1 >= A:
-            K -= 1
-    ans += 1
+# A, K = map(int, sys.stdin.readline().split())
+# ans = 0
+# while True:
+#     if A == K:
+#         print(ans)
+#         break
+#     else:
+#         if K // 2 >= A:
+#             if K % 2 == 0:
+#                 K = K // 2
+#             else:
+#                 K -= 1
+#         elif K - 1 >= A:
+#             K -= 1
+#     ans += 1
