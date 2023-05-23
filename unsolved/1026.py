27b71a71e2c68a72311fb03e1d802190e8755bff
onejuice98 <waterpurifier@khu.ac.kr>
2023-05-23 22:55:50 +0900

    project init

--- unsolved/1026.py
+++ unsolved/1026.py
@@ -1,40 +1,40 @@
-# import sys
-# from collections import deque
+import sys
+from collections import deque
 
-# N, M, V = map(int, sys.stdin.readline().split())
+N, M, V = map(int, sys.stdin.readline().split())
 
-# graph = [[0] * (N+1) for _ in range(N+1)]
+graph = [[0] * (N+1) for _ in range(N+1)]
 
-# for _ in range(M):
-#     a, b = map(int, sys.stdin.readline().split())
-#     graph[a][b] = graph[b][a] = 1
+for _ in range(M):
+    a, b = map(int, sys.stdin.readline().split())
+    graph[a][b] = graph[b][a] = 1
 
-# dfs_visited = [0] * (N+1)
-# """ dfs """
-# def dfs(root):
-#     dfs_visited[root] = 1
-#     print(root, end = " ")
-#     for i in range(1, N+1):
-#         if not dfs_visited[i] and graph[root][i]:
-#             dfs(i)
+dfs_visited = [0] * (N+1)
+""" dfs """
+def dfs(root):
+    dfs_visited[root] = 1
+    print(root, end = " ")
+    for i in range(1, N+1):
+        if not dfs_visited[i] and graph[root][i]:
+            dfs(i)
 
 
 
-# """ bfs """
+""" bfs """
 
-# def bfs(V):
-#     queue = [V]
-#     dfs_visited[V] = 0
+def bfs(V):
+    queue = [V]
+    dfs_visited[V] = 0
     
-#     while queue:
-#         V = queue.pop(0)
-#         print(V, end = " ")
-#         for i in range(1, N+1):
-#             if dfs_visited[i] == 1 and graph[V][i] == 1:
-#                 queue.append(i)
-#                 dfs_visited[i] = 0
+    while queue:
+        V = queue.pop(0)
+        print(V, end = " ")
+        for i in range(1, N+1):
+            if dfs_visited[i] == 1 and graph[V][i] == 1:
+                queue.append(i)
+                dfs_visited[i] = 0
 
 
-# dfs(V)
-# print()
-# bfs(V)
+dfs(V)
+print()
+bfs(V)
